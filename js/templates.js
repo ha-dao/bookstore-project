808d4dffc9681e287c54545ea0504b0aa29d7e00
Ha Dao <dcvh@protonmail.com>
2025-01-15 04:20:35 +0100

    render comments

--- js/templates.js
+++ js/templates.js
@@ -1,11 +1,18 @@
-function bookTemplate(book) {
-    return `
+function bookTemplate(book, bookIndex) {
+let formattedPrice = book.price.toFixed(2);
+  return `
         <div class="bookcard">
             <h3>${book.name}</h3>
             <hr>
-            <img src="">
+            <img class="book-cover" src="./assets/img/book.png">
             <hr>
-            <p class="book-price">${book.price} €</p>
+            <div class="price-likes">
+                <p class="book-price">${formattedPrice} €</p>
+                <p class="like-box" id="like-box">
+                    <svg class="basic-heart" id="liked-active" onclick="${updatedLikes(book, bookIndex)}" viewBox="-0.8 -0.8 17.60 17.60"><path d="M1.24264 8.24264L8 15L14.7574 8.24264C15.553 7.44699 16 6.36786 16 5.24264V5.05234C16 2.8143 14.1857 1 11.9477 1C10.7166 1 9.55233 1.55959 8.78331 2.52086L8 3.5L7.21669 2.52086C6.44767 1.55959 5.28338 1 4.05234 1C1.8143 1 0 2.8143 0 5.05234V5.24264C0 6.36786 0.44699 7.44699 1.24264 8.24264Z"></path></svg>
+                    ${book.likes}
+                </p>
+            </div>
             <table>
                 <tr>
                     <td>Author:</td>
@@ -21,13 +28,31 @@ function bookTemplate(book) {
                 <tr>
             </table>
             <hr>
-            <table id="comments">
             <h3>Kommentare</h3>
-                <tr>
-                    <td>Author:</td>
-                    <td></td>
-                <tr>
-            </table>
+            <input type="text" id="name-${bookIndex}" placeholder="Name" minlength="1" maxlength="12" required>
+            <textarea id="comment-${bookIndex}" placeholder="Kommentar" required></textarea>
+            <button onclick="addComment(${bookIndex})">Kommentar hinzufügen</button>
+            <div class="comment-form">
+                <table class="comments" id="comments-${bookIndex}">
+                ${renderComments(book.comments)}
+                </table>
+            </div>
         </div>
-        `
+        `;
 }
+
+function getCommentTemplate(comment) {
+  return `
+        <tr>
+            <td>${comment.name}:</td>
+            <td>${comment.comment}</td>
+        <tr>
+    `;
+}
+
+// function updatedLikesTemplate(book, bookIndex) {
+//     return `
+//         <svg class="basic-heart" id="liked-${bookIndex}"" viewBox="-0.8 -0.8 17.60 17.60"><path d="M1.24264 8.24264L8 15L14.7574 8.24264C15.553 7.44699 16 6.36786 16 5.24264V5.05234C16 2.8143 14.1857 1 11.9477 1C10.7166 1 9.55233 1.55959 8.78331 2.52086L8 3.5L7.21669 2.52086C6.44767 1.55959 5.28338 1 4.05234 1C1.8143 1 0 2.8143 0 5.05234V5.24264C0 6.36786 0.44699 7.44699 1.24264 8.24264Z"></path></svg>
+//         ${book.likes}
+//     `
+// }
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// initiazation on window load
 function init() {
     renderAllBooks();
 }
 
+// show all books from object
 function renderAllBooks() {
     let allBooks = document.getElementById('all-books');
     allBooks.innerHTML = "";
@@ -9,4 +11,38 @@ function renderAllBooks() {
     for (let bookIndex = 0; bookIndex < books.length; bookIndex++) {
         allBooks.innerHTML += bookTemplate(books[bookIndex]);
     }
+}
+
+// show all comments for each book if comment exist
+function renderComments(comments) {
+    let commentsHTML = '';
+    if (comments.length === 0) {
+        commentsHTML = '<p>Keine Kommentare vorhanden.</p>';
+    } else {
+        for (let commentIndex = comments.length - 1; commentIndex >= 0; commentIndex--) {
+            let comment = comments[commentIndex];
+            commentsHTML += getCommentTemplate(comment);
+        }
+    }
+    return commentsHTML;
+}
+
+// add new comment to existing comments box
+function addComment(comments) {
+    let nameInput = document.getElementById('name-${bookIndex}');
+    let messageInput = document.getElementById('comment-${bookIndex}');
+    let commentsHTML = '';
+
+        if(nameInput.value > 0 && messageInput.value > 0) {
+            comments.push(nameInput.value);
+            comments.push(messageInput.value);
+        } else {
+            commentsHTML = '<p>Bitte keine Felder leer lassen.</p>';
+        }
+    renderComments(comments);
+}
+
+// // update likes quantity change icon color
+function updatedLikes(bookIndex) {
+    
 }
